bb8914152c587bf09eff3ab316a0e1b12a108882
mark1985 <ellannon@gmail.com>
2017-12-30 13:49:56 +0100

    footer text slideshow

--- laravel/resources/views/partials/footer.blade.php
+++ laravel/resources/views/partials/footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="container-fluid navbar">
-	<div class="row w-100 p-2 d-flex justify-content-center">
-		<h2>Allí donde te encuentres, lleva el Islam siempre contigo</h2>
+	<div class="row w-100 p-2 d-flex justify-content-center tech-slideshow">
+		<h2 class="mover-1"> lleva el Islam siempre contigo. Allí donde te encuentres, lleva el Islam siempre contigo.</h2>
+		<h2 class="mover-2"></h2>
 	</div>
 </footer>
