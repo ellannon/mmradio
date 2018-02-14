d830381002eacb3791a4566dcec56b719239190d
Aina Bio <s-lash@ono.com>
2018-02-14 20:34:01 +0100

    cambios_aina_prueba

--- laravel/resources/views/pages/estatutos.blade.php
+++ laravel/resources/views/pages/estatutos.blade.php
@@ -5,4 +5,18 @@
 	<meta name="description" content="{!!json_decode($page->extras, true)['meta_description']!!}">
 @endsection
 @section('Estatutos')
+		<section>
+			<div class="container">
+				<div class="row mb-5">
+					<div class="col-xs-12 col-sm-12 col-md-12 m-auto pageContent">
+						<h1>{!!$page->title!!}</h1>
+					</div>
+				</div>
+				<div class="row mt-5">
+					<div class="col-xs-12 col-sm-12 col-md-8 pageContent">
+						{!!$page->content!!}
+					</div>
+				</div>
+			</div>
+		</section>
 @endsection
