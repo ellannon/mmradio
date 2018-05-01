83dbdae2826cb5d37047309ab7f71966887174ac
mark1985 <ellannon@gmail.com>
2018-05-01 21:32:55 +0200

    modal done

--- laravel/resources/views/partials/modal.blade.php
+++ laravel/resources/views/partials/modal.blade.php
@@ -2,12 +2,12 @@
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
+				<div class="modal-body">
+					<h2 style="color:#2cab79">¡Mensaje enviado correctamente!</h2>
+				</div>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
-			</div>
-			<div class="modal-body">
-				<p>¡Mensaje enviado correctamente!</p>
 			</div>
 		</div>
 	</div>
