ccb2c63ae4ea45516a68ea51b7cee5026f46f8cf
mark1985 <ellannon@gmail.com>
2018-05-01 21:40:37 +0200

    modal fixes

--- laravel/resources/views/partials/modal.blade.php
+++ laravel/resources/views/partials/modal.blade.php
@@ -1,9 +1,9 @@
 <div class="modal" tabindex="-1" role="dialog">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
-			<div class="modal-header">
-				<div class="modal-body">
-					<p style="font-size: 2rem; color:#2cab79">¡Mensaje enviado correctamente!</p>
+			<div class="modal-header" style="border-bottom:none;">
+				<div class="modal-body" style="padding:0;">
+					<span style="font-size: 1.5rem; color:#2cab79">¡Mensaje enviado correctamente!</span>
 				</div>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
