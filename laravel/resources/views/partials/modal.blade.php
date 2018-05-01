6275eb8ad646648c36b8027602968c7e51d920a9
mark1985 <ellannon@gmail.com>
2018-05-01 21:34:26 +0200

    modal font size

--- laravel/resources/views/partials/modal.blade.php
+++ laravel/resources/views/partials/modal.blade.php
@@ -3,7 +3,7 @@
 		<div class="modal-content">
 			<div class="modal-header">
 				<div class="modal-body">
-					<h2 style="color:#2cab79">¡Mensaje enviado correctamente!</h2>
+					<p style="font-size: 2rem; color:#2cab79">¡Mensaje enviado correctamente!</p>
 				</div>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
